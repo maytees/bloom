89b300037ae4b80fb6455c7743748b4f1ddbad7d
maytees <mateespublicprofile@gmail.com>
2026-07-27 22:07:52 -0400

    refactor: split reactive types into own file

--- packages/runtime/src/reactive/signal.ts
+++ packages/runtime/src/reactive/signal.ts
@@ -1,11 +1,11 @@
-import type { EffectCallback, Node, Signal } from "../types";
+import type { ReactiveNode, Signal } from "../types";
 import { Global } from "./global";
 
 export function signal<T = undefined>(defaultValue?: T): Signal<T> {
 	let value = defaultValue as T;
 	// a node is just an obj holding a signal's state.
 	// effect's use a node to identify a signal
-	const node: Node = { subscribers: new Set() };
+	const node: ReactiveNode = { subscribers: new Set() };
 
 	return (...args) => {
 		if (args.length === 0) {
--- packages/runtime/src/types.ts
+++ packages/runtime/src/types.ts
@@ -1,15 +1,3 @@
-// args for signal can either be 1 or none
-export type Signal<T = void> = (...args: T[] | [newValue: T]) => T;
-
-// is an object right now, even tho it holds 1 value.
-export type Node = { subscribers: Set<Effect> };
-
-export type EffectCallback = () => void;
-// dispose should be an object so it's clear,
-// instead of a user set var name
-export type EffectDispose = { dispose: () => void };
-export type Effect = {
-	readonly callbackFn: EffectCallback;
-	readonly run: () => void;
-	dependencies: Set<Node>;
-};
+export * from "./dom/dom";
+export * from "./reactive/types";
+export * from "./utils/types";
